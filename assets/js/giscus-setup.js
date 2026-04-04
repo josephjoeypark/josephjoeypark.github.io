9fec72e110ac53db1f3676aa1b004b222487c6cd
josephjoeypark <joeypark@mit.edu>
2026-04-04 18:29:03 +0000

    Deploying to gh-pages from @ josephjoeypark/josephjoeypark.github.io@2822d599842bf9cb43fee0bae2294d09ec5c49e5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "josephjoeypark/josephjoeypark",
+    "data-repo": "josephjoeypark/josephjoeypark.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
